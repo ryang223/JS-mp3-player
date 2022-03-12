ecab4fb50a916e70e5d601f0e1978c62e89bd944
ryang223 <ryangoodwd@gmail.com>
2022-03-12 14:43:28 -0500

    music titles and songs arrray configured within their respective folders and app JS

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -5,32 +5,32 @@ function App() {
     {
       title:"I Speed At Night",
       artist:"Dio ",
-      img_src:"./images/",
-      src:"./music/"
+      img_src:"./images/song-1.jpeg",
+      src:"./music/I-speed-at-night.mp3"
     },
     {
       title:"Vital Signs",
       artist:"Rush",
-      img_src:"./images/",
-      src:"./music/"
+      img_src:"./images/song-2.jpeg",
+      src:"./music/vital-signs.mp3"
     },
     {
       title:"Them Bones",
       artist:"Alice in Chains",
-      img_src:"./images/",
-      src:"./music/"
+      img_src:"./images/song-3.jpeg",
+      src:"./music/them-bones.mp3"
     },
     {
       title:"Message in a Bottle",
       artist:"The Police",
-      img_src:"./images/",
-      src:"./music/"
+      img_src:"./images/song-4.jpeg",
+      src:"./music/message-in-a-bottle.mp3"
     },
     {
       title:"I Ran (So Far Away)",
       artist:"A Flock of Seagulls",
-      img_src:"./images/
-      src:"./music/"
+      img_src:"./images/song-5.jpeg",
+      src:"./music/I-ran.mp3"
     },
   ])
 
